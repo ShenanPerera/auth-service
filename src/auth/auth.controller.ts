8c5e3a167627426476758714b565b4fe7f374683
ShenanPerera <shenaperera2001@gmail.com>
2024-04-28 01:21:55 +0530

    feat : authentication

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { Public } from './decorators/public.decorator';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(private authService: AuthService) {}
+
+  @HttpCode(HttpStatus.OK)
+  @Post('login')
+  signIn(@Body() signInDto: Record<string, any>) {
+    return this.authService.signIn(signInDto.username, signInDto.password);
+  }
+
+  
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable , UnauthorizedException } from '@nestjs/common';
+import { UserService } from 'src/user/user.service';
+import { JwtService } from '@nestjs/jwt';   
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(
+        private userService : UserService,
+        private jwtService : JwtService
+    ) {}
+
+    async signIn(
+        email: string, 
+        pass: string,
+    ): Promise<{access_token:string}> {
+        const user = await this.userService.findOne(email);
+        if(user?.password !== pass){
+            throw new UnauthorizedException();
+        }
+
+        const payload = {sub: user.email,username : user.fullName, role:user.role};
+
+        return{
+            access_token : await this.jwtService.signAsync(payload),
+        };
+
+        
+    }
+}
